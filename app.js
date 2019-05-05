ef5d7c15e8fe4ff30341a3b561a51020df4a0e60
bahaaib <bahaasco@gmail.com>
2019-05-05 23:06:05 +0200

    added error hadlers

--- app.js
+++ app.js
@@ -9,4 +9,22 @@ app.use(morgan('dev'));
 //Users route
 app.use('/users', usersRouter);
 
+//Not found error handler
+app.use((req, res, next) => {
+    const error = new Error('Unless you have a time machine. This page is gone!');
+    error.status = 404;
+    next(error);
+});
+
+//General error handler
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error:{
+            message: error.message
+        }
+    });
+});
+
+
 module.exports = app;
