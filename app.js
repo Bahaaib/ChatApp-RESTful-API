7bc7a33f66c6a175a485f53650a9dafbdfa611ec
bahaaib <bahaasco@gmail.com>
2019-05-06 15:01:12 +0200

    added CORS

--- app.js
+++ app.js
@@ -9,6 +9,20 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+         "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+         );
+
+         if(req.method === 'OPTIONS'){
+             res.header('Access-Control-Allow-Methods', 'PUT, PATCH, DELETE, GET, POST');
+             return res.status(200).json({});
+         }
+    next();
+});
+
 //Users route
 app.use('/users', usersRouter);
 
