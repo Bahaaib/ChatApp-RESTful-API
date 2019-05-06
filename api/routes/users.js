0d042e6b4b95058562adcfe7c371e670fc1db953
bahaaib <bahaasco@gmail.com>
2019-05-06 14:47:38 +0200

    added body parsers

--- api/routes/users.js
+++ api/routes/users.js
@@ -3,13 +3,20 @@ const router = express.Router();
 
 router.get('/', (req, res, next) => {
     res.status(200).json({
-        message: 'Processing your GET request to /users'
+        message: 'Processing your GET request to /users'       
     });
 });
 
 router.post('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Processing your POST request to /users'
+
+    const user = {
+        name: req.body.name,
+        email: req.body.email
+    };
+
+    res.status(201).json({
+        message: 'Processing your POST request to /users',
+        currentUser: user
     });
 });
 
