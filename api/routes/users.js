93d7e051c7bd3a29be787fb0e5dd52ce3756dce8
bahaaib <bahaasco@gmail.com>
2019-05-09 22:03:22 +0200

    moved models to api folder

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const User = require('../')
+
 router.get('/', (req, res, next) => {
     res.status(200).json({
         message: 'Processing your GET request to /users'       
