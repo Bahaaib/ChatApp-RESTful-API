db4139cc7d38bb8f6d0c51da33b80f55edf86dc8
bahaaib <bahaasco@gmail.com>
2019-05-17 03:41:38 +0200

    modified user schema

--- api/models/users.js
+++ api/models/users.js
@@ -2,8 +2,9 @@ const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    name: String,
-    email: String,
+    name: { type: String, require: true },
+    email: { type: String, require: true },
+    user_avatar: { type: String }
 });
 
 module.exports = mongoose.model('User', userSchema);
