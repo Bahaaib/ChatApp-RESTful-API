cba9cf8959fa52457261ac5c1466aec383746f8f
bahaaib <bahaasco@gmail.com>
2019-05-20 22:36:41 +0200

    added user signup to controller

--- api/routes/users.js
+++ api/routes/users.js
@@ -5,6 +5,7 @@ const multer = require('multer');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const checkAuth = require('../middleware/check-auth');
+const userController = require('../controllers/users');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -30,74 +31,8 @@ const upload = multer({
     fileFilter: fileFilter
 });
 
-const User = require('../models/users');
-
 //Create User with profile avatar in DB
-router.post("/signup", upload.single('user_avatar'), (req, res, next) => {
-    User.find({ email: req.body.email })
-        .exec()
-        .then(user => {
-            if (user.length >= 1) {
-                return res.status(409).json({
-                    message: 'E-mail already exists'
-                });
-            } else {
-                const token = jwt.sign(
-                    {
-                        email: req.body.email,
-                        _id: req.body._id
-                    },
-                    process.env.JWT_KEY,
-                    {
-                        expiresIn: "1h"
-                    }
-                );
-                bcrypt.hash(req.body.password, 10, (err, hash) => {
-                    if (err) {
-                        return res.status(500).json({
-                            error: err
-                        })
-                    } else {
-                        if (req.file) {
-                            user = new User({
-                                _id: new mongoose.Types.ObjectId,
-                                name: req.body.name,
-                                email: req.body.email,
-                                password: hash,
-                                user_avatar: req.file.path
-                            });
-                        } else {
-                            user = new User({
-                                _id: new mongoose.Types.ObjectId,
-                                name: req.body.name,
-                                email: req.body.email,
-                                password: hash,
-                            });
-                        }
-
-
-                        user
-                            .save()
-                            .then(result => {
-                                console.log(result)
-                                res.status(201).json({
-                                    message: 'Processing your SIGN UP..',
-                                    currentUser: user,
-                                    token: token
-                                });
-                            })
-                            .catch(err => {
-                                console.log(err);
-                                res.status(500).json({
-                                    error: err
-                                });
-                            });
-                    }
-                })
-            }
-        });
-
-});
+router.post("/signup", upload.single('user_avatar'), userController.signup_user);
 
 //Login users to DB
 router.post("/login", (req, res, next) => {
